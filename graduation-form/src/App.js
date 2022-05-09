2bd88487febbf732a616099bd483a0c7ec1a6b98
Jeffrey Dick <jeffrey@jeffreydick.com>
2022-05-09 13:21:03 +1200

    Decode jwt

--- graduation-form/src/App.js
+++ graduation-form/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { isExpired, decodeToken } from "react-jwt";
 import "./App.css";
 import { client } from "./ApolloClient/client";
 import { ApolloProvider } from "@apollo/client";
 
 function App({ element }) {
+  const token = new URLSearchParams(window.location.search).get("token");
+  const myDecodedToken = decodeToken(token);
+  if (isExpired(token)) {
+    return (window.location.href =
+      "https://jeffrey.op.ac.nz/tokens/token?ignore-saml-redirect=1");
+  }
+  console.log(myDecodedToken);
   const graphqlURL = element.getAttribute("data-graphql-url");
   return (
     <div className="graduation-form">
       <ApolloProvider client={client(graphqlURL)}>
-        TEST
+        Graduation Form
       </ApolloProvider>
     </div>
   );
